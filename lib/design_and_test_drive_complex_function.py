89ddcd795a31875b027bbc53e2ff84d4c01a8bc8
Charlotte Handford <handford.charlotte@yahoo.co.uk>
2025-06-12 15:56:18 +0100

    Abstracted DOB validation logic and exceptions for incorrect format

--- lib/design_and_test_drive_complex_function.py
+++ lib/design_and_test_drive_complex_function.py
@@ -1,17 +1,28 @@
-from datetime import datetime, timedelta
-# import dateutil 
+from datetime import datetime
+
+def dob_format_checker(date_of_birth):
+    if len(date_of_birth) == 10 and date_of_birth[4] == '-':
+        year = date_of_birth[0:4]
+        month = date_of_birth[5:7]
+        day = date_of_birth[8:10]
+        return year.isnumeric() and month.isnumeric() and day.isnumeric()
+
+    return False
 
 def check_age(date_of_birth):
-    today_dt = datetime.today()
-    dob_dt = datetime.strptime(date_of_birth, "%Y-%m-%d")
+    if type(date_of_birth) == str:
+            if dob_format_checker(date_of_birth) == True:
+        
+                today_dt = datetime.today()
+                dob_dt = datetime.strptime(date_of_birth, "%Y-%m-%d")
 
-    age_dt = today_dt - dob_dt
-    total_days = age_dt.days
+                age_dt = today_dt - dob_dt
+                total_days = age_dt.days
 
-    user_age = total_days // 365
+                user_age = total_days // 365
 
-    if user_age > 16:
-        return "Access granted!"
-    return f"Access denied! Current age: {user_age}. The required age is 16."
-
-    # generates an exception when the date of birth isn't the right type/format
+                if user_age > 16:
+                    return "Access granted!"
+                return f"Access denied! Current age: {user_age}. The required age is 16."
+            
+    raise Exception("Date of birth in incorrect format. Provide a string in the format `YYYY-MM-DD`")
--- tests/test_design_and_test_drive_complex_function.py
+++ tests/test_design_and_test_drive_complex_function.py
@@ -1,25 +1,41 @@
-from lib.design_and_test_drive_complex_function import check_age
+from lib.design_and_test_drive_complex_function import check_age, dob_format_checker
+
 import pytest 
-import datetime
+from datetime import datetime
+
+def test_dob_format_checker():
+    assert dob_format_checker("1993-10-21") == True
+    assert dob_format_checker("1993") == False
+    assert dob_format_checker("21-10-1993") == False
+
 
 def test_check_age_returns_access_granted_message_when_user_is_over_16():
     assert check_age("1993-10-21") == "Access granted!"
+    assert check_age("2005-01-13") == "Access granted!"
+
 
 def test_check_age_returns_access_denied_message_when_user_is_under_16():
     assert check_age("2014-01-01") == "Access denied! Current age: 11. The required age is 16."
+    assert check_age("2010-01-01") == "Access denied! Current age: 15. The required age is 16."
 
+def test_check_age_raises_exception_when_dob_of_wrong_type():
+    with pytest.raises(Exception) as err:
+        check_age(18) # int
+    error_message = str(err.value)
+    assert error_message == "Date of birth in incorrect format. Provide a string in the format `YYYY-MM-DD`"
 
-"""
-Given a DOB of the wrong type,
-it returns an empty list
-"""
-# check_age(datetime.today()) 
-# # => "Date of birth in incorrect format. Provide a string in the format `YYYY-MM-DD`"
+    with pytest.raises(Exception) as err:
+        check_age(datetime.today()) #datetime object
+    error_message = str(err.value)
+    assert error_message == "Date of birth in incorrect format. Provide a string in the format `YYYY-MM-DD`"
 
+def test_check_age_raises_exception_when_in_wrong_format():
+    with pytest.raises(Exception) as err:
+        check_age("2001")
+    error_message = str(err.value)
+    assert error_message == "Date of birth in incorrect format. Provide a string in the format `YYYY-MM-DD`"
 
-"""
-Given a DOB in the wrong format,
-it returns an empty list
-"""
-# check_age("01-01-2024") 
-# # => "Date of birth in incorrect format. Provide a string in the format `YYYY-MM-DD`"
+    with pytest.raises(Exception) as err:
+        check_age("01-02-2001")
+    error_message = str(err.value)
+    assert error_message == "Date of birth in incorrect format. Provide a string in the format `YYYY-MM-DD`"
